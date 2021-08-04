7b2dfad67c81d8b8aeca1d92d14a9666f831cb60
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-04 19:32:18 -0300

    update

--- projeto-atual/ceep/src/index.js
+++ projeto-atual/ceep/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './teste.css';
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App></App>
   </React.StrictMode>,
   document.getElementById('root')
 );
