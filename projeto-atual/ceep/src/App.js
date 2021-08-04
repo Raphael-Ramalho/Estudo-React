4fe0275a68572952d1831f57b65c1569402f4878
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-03 23:08:06 -0300

    Update App.js

--- projeto-atual/ceep/src/App.js
+++ projeto-atual/ceep/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-function App() {
+function App() {// o App() deve devolver apenas um elemento. dessa forma, o form e o ul devem estar dentro de um container qualquer(devem estar encapsulados)
   return (
-    <form>
-      <input type = "text" placeholder="Titulo"></input>
-      <textarea placeholder="Escreva sua nota..."></textarea>
-      <button>Criar Nota</button>
-    </form>
+    <section>
+      <form>
+        <input type = "text" placeholder="Titulo"></input>
+        <textarea placeholder="Escreva sua nota..."></textarea>
+        <button>Criar Nota</button>
+      </form>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+            <p>Escreva sua nota</p>
+          </section>
+        </li>
+      </ul>
+    </section>
   );
 }
 
