16f2f47016ecf2297a5451a3273fe90cf2b29513
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-03 23:17:23 -0300

    Update App.js

--- projeto-atual/ceep/src/App.js
+++ projeto-atual/ceep/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ListaDeNotas } from './components/listaDeNotas.jsx';
 
-function App() {// o App() deve devolver apenas um elemento. dessa forma, o form e o ul devem estar dentro de um container qualquer(devem estar encapsulados). o App é chamado de componente
+function App() {// o App() deve devolver apenas um elemento. dessa forma, o form e o ul devem estar dentro de um container qualquer(devem estar encapsulados). o ListaDeNotas é um componente
   return (
     <section>
       <form>
