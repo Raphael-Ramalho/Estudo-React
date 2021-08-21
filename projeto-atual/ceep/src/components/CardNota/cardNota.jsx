f66719233f41e1d6b4210af6dfa7b8de45e5f7da
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-21 18:57:08 -0300

    update

--- projeto-atual/ceep/src/components/CardNota/cardNota.jsx
+++ projeto-atual/ceep/src/components/CardNota/cardNota.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import "./estilo.css"
+import {ReactComponent as DeleteSVG} from "../../assets/img/delete.svg"
 
 class CardNota extends Component {
     render() {
@@ -7,11 +8,12 @@ class CardNota extends Component {
             <section className="card-nota">
                 <header className="card-nota_cabecalho">
                     <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+                    <DeleteSVG onClick = {()=>console.log("deletar")}/>
                 </header>
                 <p className="card-nota_texto">{this.props.texto}</p>
             </section>
         );
     }
-}
+} 
 
 export default CardNota;
