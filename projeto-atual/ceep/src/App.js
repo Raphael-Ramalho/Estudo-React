130fbffca601b602845e0b4739f15dc0ff9ee876
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-21 17:26:21 -0300

    Update App.js

--- projeto-atual/ceep/src/App.js
+++ projeto-atual/ceep/src/App.js
@@ -7,20 +7,25 @@ import './assets/index.css';
 class App extends Component {// o App() deve devolver apenas um elemento. dessa forma, o form e o ul devem estar dentro de um container qualquer(devem estar encapsulados). o ListaDeNotas é um componente
   constructor(){
     super()
-    this.notas = []
-    this.state ={} 
+    this.state = {
+      notas:[]
+    } //o state é uma funcionalidade basica do react assim como o props. Ele é responsavel por atualizar a visualização dos componentes. Nós desenvolvedores não podemos chamar o metodo render a qualquer momento, mas o react o chama sempre que o state de allgum componente é alterado. Normalmente usamos o state como um objeto {} pq podemos adicionar e remover coisas facilmente 
   }
   
   criarNota(titulo, texto){
     const novaNota = {titulo, texto}
-    this.notas.push(novaNota)
-    console.log("notas", this.notas)
+    const novoArrayNotas = [...this.state.notas, novaNota] // similar a const novoArrayNotas = this.state.notas.push(novaNota) 
+    const novoEstado ={
+      notas: novoArrayNotas
+    }
+    this.setState(novoEstado) //o estado é alterado com o metodo setState e como parâmetro, devemos passar o novo estado do componente
   }
+
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota = {this.criarNota.bind(this)}/>
-        <ListaDeNotas notas = {this.notas}/>
+        <ListaDeNotas notas = {this.state.notas}/>
       </section>
     );
   }
