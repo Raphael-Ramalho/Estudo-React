46bd037d3781b641841fc71ee4eb7aef215331fd
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-08-22 12:01:37 -0300

    update

--- projeto-atual/ceep/src/App.js
+++ projeto-atual/ceep/src/App.js
@@ -1,45 +1,59 @@
 import React, { Component } from 'react';
 import ListaDeNotas from './components/ListaDeNotas';
 import FormularioCadastro from "./components/FormularioCadastro"
+import ListaDeCategorias from './components/ListaDeCategorias/listaDeCategorias';
 import "./assets/App.css"
 import './assets/index.css';
 
 class App extends Component {// o App() deve devolver apenas um elemento. dessa forma, o form e o ul devem estar dentro de um container qualquer(devem estar encapsulados). o ListaDeNotas é um componente
-  constructor(){
+  constructor() {//o state é uma funcionalidade basica do react assim como o props. Ele é responsavel por atualizar a visualização dos componentes. Nós desenvolvedores não podemos chamar o metodo render a qualquer momento, mas o react o chama sempre que o state de allgum componente é alterado. Normalmente usamos o state como um objeto {} pq podemos adicionar e remover coisas facilmente 
     super()
     this.state = {
-      notas:[]
-    } //o state é uma funcionalidade basica do react assim como o props. Ele é responsavel por atualizar a visualização dos componentes. Nós desenvolvedores não podemos chamar o metodo render a qualquer momento, mas o react o chama sempre que o state de allgum componente é alterado. Normalmente usamos o state como um objeto {} pq podemos adicionar e remover coisas facilmente 
+      notas: [],
+      categorias: []
+    } 
   }
-  
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto}
+
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto }
     const novoArrayNotas = [...this.state.notas, novaNota] // similar a const novoArrayNotas = this.state.notas.push(novaNota) 
-    const novoEstado ={
+    const novoEstado = {
       notas: novoArrayNotas
     }
     this.setState(novoEstado) //o estado é alterado com o metodo setState e como parâmetro, devemos passar o novo estado do componente
   }
 
-  deletarNota(index){
+  adicionarCategoria(nomeCategoria){
+    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
+    const novoEstado = {
+      categorias: novoArrayCategorias
+    }
+    this.setState(novoEstado)
+  }
+
+  deletarNota(index) {
     let arrayNotas = this.state.notas
-    console.log("arrayNotas: ", arrayNotas)
-    arrayNotas.splice(index,1)
-    console.log("index: ",index)
-    this.setState({notas:arrayNotas})
+    arrayNotas.splice(index, 1)
+    this.setState({ notas: arrayNotas })
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota = {this.criarNota.bind(this)}/>
-        <ListaDeNotas 
-          apagarNota = {this.deletarNota.bind(this)}
-          notas = {this.state.notas}
-        />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <main className="conteudo-principal">
+          <ListaDeCategorias 
+            adicionarCategoria = {this.adicionarCategoria.bind(this)}
+            categorias = {this.state.categorias}
+          />
+          <ListaDeNotas
+            apagarNota={this.deletarNota.bind(this)}
+            notas={this.state.notas}
+          />
+        </main>
       </section>
     );
   }
 }
 
-export default App;
+export default App; 
---
+++ projeto-atual/ceep/src/components/ListaDeCategorias/listaDeCategorias.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import "./estilo.css"
+
+class ListaDeCategorias extends Component {
+
+  _handlerEventoInput(e){
+    if(e.key === "Enter"){
+      let valorCategoria = e.target.value
+      this.props.adicionarCategoria(valorCategoria)
+      e.target.value = ""
+    }
+  }
+
+  render() {
+    return (
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+            {this.props.categorias.map((categoria, index)=>{
+              return <li key = {index} className="lista-categorias_item">{categoria}</li>
+            })}
+          
+        </ul>
+        <input
+          type="text"
+          className="lista-categorias_input" 
+          placeholder="Adicionar Categoria"
+          onKeyUp={this._handlerEventoInput.bind(this)}
+        />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
+
+
