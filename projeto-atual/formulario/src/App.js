1a73c3f73e127f6fb56ef1647dfca37aef1083c0
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-09-08 09:38:37 -0300

    update

--- projeto-atual/formulario/src/App.js
+++ projeto-atual/formulario/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import FormularioCadastro from './components/formularioCadastro/formularioCadastro';
 
 class App extends Component {
   render(){
     return (
-      <h1>Formulário de Cadastro</h1>
+      <>
+        <h1>Formulário de Cadastro</h1>
+        <FormularioCadastro/>
+      </>
     );
   }
 }
