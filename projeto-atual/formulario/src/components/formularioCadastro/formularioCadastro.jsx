86b23d19e4e2da37a437ded1ba93228df0ddc8d2
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-09-11 11:38:59 -0300

    Update formularioCadastro.jsx

--- projeto-atual/formulario/src/components/formularioCadastro/formularioCadastro.jsx
+++ projeto-atual/formulario/src/components/formularioCadastro/formularioCadastro.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 function FormularioCadastro(){
     return(
-        <form><input type="text" /></form>
+        <form>
+            <label>Nome</label>
+            <input type="text" />
+            
+            <label>Sobrenome</label>
+            <input type="text" />
+            
+            <label>CPF</label>
+            <input type="text" />
+
+            <label>Promoções</label>
+            <input type="checkbox" />
+        
+            <label>Novidades</label>
+            <input type="checkbox" />
+
+            <button type='submit'>Cadastrar</button>
+        </form>
     );
 }
 
