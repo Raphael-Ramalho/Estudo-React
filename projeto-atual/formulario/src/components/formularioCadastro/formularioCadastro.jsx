ddfff6cecc2c0a438118de0e6e8f7cf623790a84
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-09-12 14:42:47 -0300

    Update formularioCadastro.jsx

--- projeto-atual/formulario/src/components/formularioCadastro/formularioCadastro.jsx
+++ projeto-atual/formulario/src/components/formularioCadastro/formularioCadastro.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core"
 
 function FormularioCadastro() {
-  let nome =""
-  
+  const [nome, setNome] = useState("");//O use State devolve uma variável para referenciar qual o estado que está dentro do componente
+  const [sobrenome, setSobrenome] = useState("")
+
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        console.log(nome, sobrenome)
+      }
+      }>
       <TextField
-        onChange={event =>{console.log(event.target)}}
+        value={nome}
+        onChange={event => {
+          let tmpNome = event.target.value
+          if (tmpNome.length >= 3) {
+            tmpNome = tmpNome.substr(0, 3)
+          }
+          setNome(tmpNome)
+        }}
         id="nome"
         label="Nome"
         color="secondary"
@@ -16,6 +29,10 @@ function FormularioCadastro() {
         margin="normal"
       />
       <TextField
+        value={sobrenome}
+        onChange={event => {
+          setSobrenome(event.target.value)
+        }}
         id="sobrenome"
         label="Sobrenome"
         color="secondary"
@@ -35,9 +52,9 @@ function FormularioCadastro() {
         control={<Switch name="promocoes" defaultChecked color="primary" />}
       />
 
-      <FormControlLabel 
-        label="Novidades" 
-        control={<Switch name="novidades" defaultChecked color="primary" />} 
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
       />
 
       <Button type='submit' variant="contained" color="primary">Cadastrar</Button>
