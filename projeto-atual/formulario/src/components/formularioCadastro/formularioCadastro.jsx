6ef1e94430cf77c80b4cc0febdf3d05af7c58508
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-09-17 19:54:51 -0300

    update

--- projeto-atual/formulario/src/components/formularioCadastro/formularioCadastro.jsx
+++ projeto-atual/formulario/src/components/formularioCadastro/formularioCadastro.jsx
@@ -4,14 +4,14 @@ import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
 
-function FormularioCadastro({ aoEnviar, validarCPF }) {
+function FormularioCadastro({ aoEnviar, validacoes }) {
   const [etapaAtual, setEtapaAtual] = useState(0);
   const [dadosColetados, setDados] = useState({});
 
   const formularios = [
-    <DadosUsuario aoEnviar={coletarDados} />,
-    <DadosPessoais aoEnviar={coletarDados} validarCPF={validarCPF} />,
-    <DadosEntrega aoEnviar={coletarDados} />,
+    <DadosUsuario aoEnviar={coletarDados} validacoes={validacoes} />,
+    <DadosPessoais aoEnviar={coletarDados} validarCPF={validacoes} />,
+    <DadosEntrega aoEnviar={coletarDados} validarCPF={validacoes} />,
     <Typography variant="h5">Obrigado pelo cadastro!</Typography>,
   ];
 
