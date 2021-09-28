bceef3cb65cc5c43a931499a5c21a52aa04edf17
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-09-27 21:47:21 -0300

    Update ValidacoesCadastro.js

--- projeto-atual/formulario/src/contexts/ValidacoesCadastro.js
+++ projeto-atual/formulario/src/contexts/ValidacoesCadastro.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const ValidacoesCadastro = React.createContext()
+const ValidacoesCadastro = React.createContext({  //definindo uma validação padrão
+  cpf: semValidacao, 
+  senha: semValidacao, 
+  nome: semValidacao 
+})
+
+function semValidacao(dados) {
+  console.log(dados)
+  return { valido: true, texto: "" }
+}
 
 export default ValidacoesCadastro
