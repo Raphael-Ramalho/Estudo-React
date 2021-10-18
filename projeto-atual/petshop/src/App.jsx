e97e4fa8fbad0beaa1cdeea2f9b76aebfb40a1e2
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-10-17 23:27:44 -0300

    update

---
+++ projeto-atual/petshop/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path="/sobre">
+          <Sobre />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default App
