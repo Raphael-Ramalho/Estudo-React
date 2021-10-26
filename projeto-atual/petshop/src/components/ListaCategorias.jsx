7aed55a25863a577323c4f5307c0714e107067ec
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-10-26 19:59:06 -0300

    update

--- projeto-atual/petshop/src/components/ListaCategorias.jsx
+++ projeto-atual/petshop/src/components/ListaCategorias.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
-import api, { busca } from '../api/api'
+import { busca } from '../api/api'
 import '../assets/css/blog.css'
 
 const ListaCategorias = () =>{
