a0f52c1cb3803502d1dde1b1044c55dc2b795baf
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-11-03 19:59:55 -0300

    update

--- projeto-atual/petshop/src/paginas/Categoria.jsx
+++ projeto-atual/petshop/src/paginas/Categoria.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import ListaCategorias from '../components/ListaCategorias'
 import '../assets/css/blog.css'
 import ListaPost from '../components/ListaPost'
-import { useParams } from 'react-router'
-import { Route } from 'react-router-dom'
+import { Route, useRouteMatch, useParams } from 'react-router-dom'
 
 const Categoria = () => {
     const { id } = useParams()
+    const { path } = useRouteMatch()
     return (
         <>
             <div className="container">
                 <h2 className="titulo-pagina">Pet Notícias</h2>
             </div>
             <ListaCategorias />
-            <Route>
+            <Route exact path={`${path}/`}>
                 <ListaPost url={`/posts/?categoria=${id}`} />
             </Route>
         </>
