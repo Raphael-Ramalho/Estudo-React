f954d9f239d1c433353914c6bedd3815f17f6ad6
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-11-07 15:21:58 -0300

    update

--- projeto-atual/petshop/src/paginas/Categoria.jsx
+++ projeto-atual/petshop/src/paginas/Categoria.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ListaCategorias from '../components/ListaCategorias'
 import '../assets/css/blog.css'
 import ListaPost from '../components/ListaPost'
-import { Route, useRouteMatch, useParams } from 'react-router-dom'
+import { Route, useRouteMatch, useParams, Link } from 'react-router-dom'
+import { busca } from '../api/api'
 
 const Categoria = () => {
     const { id } = useParams()
-    const { path } = useRouteMatch()
-    
+    const { url, path } = useRouteMatch()
+    const [subCategorias, setSubCategorias] = useState([])
+
+    useEffect(() => {
+        busca(`/categorias/${id}`, (categoria) => {
+            setSubCategorias(categoria.subCategorias)
+        })
+    }, [id])
+
     return (
         <>
             <div className="container">
                 <h2 className="titulo-pagina">Pet Notícias</h2>
             </div>
             <ListaCategorias />
+            <ul className="lista-categorias container flex">
+                {
+                    subCategorias.map((subcategoria) => (
+                        <li className={`lista-categorias__categoria lista-categorias__categoria--${id}`} key={subcategoria}>
+                            <Link to={`${url}/${subcategoria}`}>
+                                {subcategoria}
+                            </Link>
+                        </li>
+                    ))
+
+                }
+            </ul>
             <Route exact path={`${path}/`}>
                 <ListaPost url={`/posts?categoria=${id}`} />
             </Route>
--- projeto-atual/petshop/src/paginas/SubCategorias.jsx
+++ projeto-atual/petshop/src/paginas/SubCategorias.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router'
+import ListaPost from '../components/ListaPost'
 
 const SubCategorias = () => {
+  const {subcategoria} = useParams()
   return(
-
+    <ListaPost url={`/post?subcategorias=${subcategoria}`} />
   )
 }
 
