3d8f12c23872f8d48ca1443ea3b8ca8b7b744228
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-12-21 21:14:49 -0300

    update

--- projeto-atual/e-commerce/src/Header.js
+++ projeto-atual/e-commerce/src/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import Logo from "./assets/img/amazon-logo.JPG";
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
@@ -9,6 +11,7 @@ function Header() {
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon"/>
       </div>
 
       <div className="header__nav">
@@ -24,6 +27,11 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
           <span className="header__optionLineTwo">22997654340</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon/>
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
