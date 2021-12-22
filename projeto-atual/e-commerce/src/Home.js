a16eeb8a131f30e96ea4ae8d833de0d88e685cb8
Raphael-Ramalho <rsramalhovf@gmail.com>
2021-12-22 15:47:20 -0300

    update

--- projeto-atual/e-commerce/src/Home.js
+++ projeto-atual/e-commerce/src/Home.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css"
 import Carousel from "./components/Carousel.jsx";
 
+import slide1 from "./assets/img/carousel/61ASx7NHTWL._SX3000_.jpg";
+import slide2 from "./assets/img/carousel/61CX1noQ8nL._SX3000_.jpg";
+import slide3 from "./assets/img/carousel/61DUO0NqyyL._SX3000_.jpg";
+import slide4 from "./assets/img/carousel/71G7vKHnFZL._SX3000_.jpg";
+import slide5 from "./assets/img/carousel/711Y9Al9RNL._SX3000_.jpg";
+
 function Home(){
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const slideImages = [slide1, slide2, slide3, slide4, slide5];
+
+  const handleImage = (value) => {
+    setCurrentImage(value)
+  }
+
   return(
     <div className="home">
-      <div className="home__container">
-        <Carousel/>
+      <div className="home__container" style={{
+        backgroundImage: `url(${slideImages[currentImage]})`
+      }}>
+        <Carousel slideImages={slideImages} currentImage={currentImage} handleImage={handleImage}/>
       </div>
     </div>
   )
--- projeto-atual/e-commerce/src/components/Carousel.jsx
+++ projeto-atual/e-commerce/src/components/Carousel.jsx
@@ -1,46 +1,45 @@
-import React, { useState } from 'react';
-import slide1 from '../assets/img/carousel/61ASx7NHTWL._SX3000_.jpg' 
-import slide2 from '../assets/img/carousel/61CX1noQ8nL._SX3000_.jpg' 
-import slide3 from '../assets/img/carousel/61DUO0NqyyL._SX3000_.jpg' 
-import slide4 from '../assets/img/carousel/71G7vKHnFZL._SX3000_.jpg' 
-import slide5 from '../assets/img/carousel/711Y9Al9RNL._SX3000_.jpg' 
+import React from "react";
+import { Button } from "@mui/material";
+import "./Carousel.css";
 
-function Carousel () {
-  const [currentImage, setCurrentImage] = useState(0)
 
-  const slideImages = [slide1, slide2, slide3, slide4, slide5]
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+function Carousel({slideImages, currentImage, handleImage}) {
+  // const [currentImage, setCurrentImage] = useState(0);
+
+  // const slideImages = [slide1, slide2, slide3, slide4, slide5];
 
   const nextImage = () => {
-    if(currentImage + 1 === slideImages.length){
-      setCurrentImage(0)
+    if (currentImage + 1 === slideImages.length) {
+      handleImage(0);
     } else {
-      setCurrentImage(currentImage + 1)
+      handleImage(currentImage + 1);
     }
-  }
+  };
 
   const previousImage = () => {
-    if(currentImage === 0){
-      setCurrentImage(slideImages.length - 1)
+    if (currentImage === 0) {
+      handleImage(slideImages.length - 1);
     } else {
-      setCurrentImage(currentImage - 1)
+      handleImage(currentImage - 1);
     }
-  }
+  };
 
-  return(
-    <div>
+  return (
+    <div
+      className="container"
+    >
+      <Button variant="text" className="button" onClick={() => previousImage()}>
+        <ArrowBackIosNewIcon className="icon" />
+      </Button>
 
-      <button
-        onClick={()=>previousImage()}
-      >Previous</button>
-
-      <button
-        onClick={()=>nextImage()}
-      >Next</button>
-
-      <img src={slideImages[currentImage]} alt="" />
-      
+      <Button variant="text" className="button" onClick={() => nextImage()}>
+        <ArrowForwardIosIcon className="icon" />
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
