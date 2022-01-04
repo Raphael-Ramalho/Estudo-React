9c5a1e99243fea374c24f22cfa01b182a8ee3da8
Raphael-Ramalho <rsramalhovf@gmail.com>
2022-01-03 22:15:59 -0300

    update

--- projeto-atual/e-commerce/src/pages/home/Home.js
+++ projeto-atual/e-commerce/src/pages/home/Home.js
@@ -40,7 +40,7 @@ function Home() {
     <div
       className="home"
       style={{
-        backgroundImage: `url(${slideImages[currentImage]})`,
+        backgroundImage: `linear-gradient(0deg, rgb(204, 204, 204),  rgba(255,0,0,0) 40%), url(${slideImages[currentImage]})`,
       }}
     >
       <div className="home__container">
