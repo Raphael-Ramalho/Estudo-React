38cbe9febf8337f1faf8a41935717ad8d2039f8c
Raphael-Ramalho <rsramalhovf@gmail.com>
2022-01-11 16:49:10 -0300

    update

--- projeto-atual/e-commerce/src/components/subtotal/Subtotal.jsx
+++ projeto-atual/e-commerce/src/components/subtotal/Subtotal.jsx
@@ -9,8 +9,7 @@ function Subtotal() {
         renderText={(value) => (
           <>
             <p>
-              Subtotal({basket.length} items):
-              <strong>{`${value}`}</strong>
+              Subtotal (0 items): <strong>{0}</strong>
             </p>
             <small className="subtotal__gift">
               <input type='checkbox'/> This order contains a gift
